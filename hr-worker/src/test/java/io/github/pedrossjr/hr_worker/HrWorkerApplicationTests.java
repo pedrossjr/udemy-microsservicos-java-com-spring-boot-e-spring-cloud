c51b7d4342e965bf0b7832c67efe5daed5ccfe0d
pedrossjr <pedrossjr@yahoo.com.br>
2025-12-30 23:16:11 -0300

    project hr-worker created

---
+++ hr-worker/src/test/java/io/github/pedrossjr/hr_worker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pedrossjr.hr_worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
