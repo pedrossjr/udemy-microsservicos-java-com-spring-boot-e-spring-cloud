d31d99aac482b778825eb3b53ef1049be52a73f0
pedrossjr <pedrossjr@yahoo.com.br>
2025-12-31 00:09:04 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/io/github/pedrossjr/hr_payroll/services/PaymentService.java
@@ -0,0 +1,12 @@
+package io.github.pedrossjr.hr_payroll.services;
+
+import io.github.pedrossjr.hr_payroll.entities.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+  public Payment getPayment(long workerId, int days) {
+    return new Payment("Bob", 200.0, days);
+  }
+}
