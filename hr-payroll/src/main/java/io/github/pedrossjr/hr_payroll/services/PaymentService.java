a314e3406feac92e0b8e0b95d8d0ac563aac52bb
pedrossjr <pedrossjr@yahoo.com.br>
2025-12-31 00:30:04 -0300

    RestTemplate

--- hr-payroll/src/main/java/io/github/pedrossjr/hr_payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/io/github/pedrossjr/hr_payroll/services/PaymentService.java
@@ -1,12 +1,32 @@
 package io.github.pedrossjr.hr_payroll.services;
 
 import io.github.pedrossjr.hr_payroll.entities.Payment;
+import io.github.pedrossjr.hr_payroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import rx.Scheduler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
-  public Payment getPayment(long workerId, int days) {
-    return new Payment("Bob", 200.0, days);
-  }
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public Payment getPayment(long workerId, int days) {
+
+        Map<String, String> uriVariables = new HashMap<>();
+
+        uriVariables.put("id", ""+workerId);
+
+        Worker worker = restTemplate.getForObject(workerHost + "workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
+    }
 }
