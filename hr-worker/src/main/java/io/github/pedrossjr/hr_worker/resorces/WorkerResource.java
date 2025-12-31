803fcf6d382a9f3fd5598f91db29df3b29050761
pedrossjr <pedrossjr@yahoo.com.br>
2025-12-31 00:56:42 -0300

    Rebbon load balancing

--- hr-worker/src/main/java/io/github/pedrossjr/hr_worker/resorces/WorkerResource.java
+++ hr-worker/src/main/java/io/github/pedrossjr/hr_worker/resorces/WorkerResource.java
@@ -2,7 +2,10 @@ package io.github.pedrossjr.hr_worker.resorces;
 
 import io.github.pedrossjr.hr_worker.entities.Worker;
 import io.github.pedrossjr.hr_worker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,18 +18,25 @@ import java.util.List;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
-  @Autowired
-  private WorkerRepository repository;
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
-  @GetMapping
-  public ResponseEntity<List<Worker>> findall() {
-    List<Worker> list = repository.findAll();
-    return ResponseEntity.ok(list);
-  }
+    @Autowired
+    private Environment env;
 
-  @GetMapping(value = "/{id}")
-  public ResponseEntity<Worker> findById(@PathVariable Long id) {
-    Worker obj = repository.findById(id).get();
-    return ResponseEntity.ok(obj);
-  }
+    @Autowired
+    private WorkerRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<Worker>> findall() {
+        List<Worker> list = repository.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        logger.info("PORT = " + env.getProperty("local.server.port"));
+
+        Worker obj = repository.findById(id).get();
+        return ResponseEntity.ok(obj);
+    }
 }
