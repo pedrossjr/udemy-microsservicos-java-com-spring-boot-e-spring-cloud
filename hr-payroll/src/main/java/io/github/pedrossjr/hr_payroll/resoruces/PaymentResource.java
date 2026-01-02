93711c577f5443738e948ff4eb12ad807499c852
pedrossjr <pedrossjr@yahoo.com.br>
2026-01-02 11:27:26 -0300

    Histrix fault tolerance

--- hr-payroll/src/main/java/io/github/pedrossjr/hr_payroll/resoruces/PaymentResource.java
+++ hr-payroll/src/main/java/io/github/pedrossjr/hr_payroll/resoruces/PaymentResource.java
@@ -1,5 +1,6 @@
 package io.github.pedrossjr.hr_payroll.resoruces;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import io.github.pedrossjr.hr_payroll.entities.Payment;
 import io.github.pedrossjr.hr_payroll.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/payments")
 public class PaymentResource {
 
-  @Autowired
-  private PaymentService service;
+    @Autowired
+    private PaymentService service;
 
-  @GetMapping(value = "/{workerId}/days/{days}")
-  public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
+    @GetMapping(value = "/{workerId}/days/{days}")
+    public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
     Payment payment = service.getPayment(workerId, days);
     return ResponseEntity.ok(payment);
-  }
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+        Payment payment = new Payment("Brann", 400.0, days);
+        return ResponseEntity.ok(payment);
+    }
 
 }
